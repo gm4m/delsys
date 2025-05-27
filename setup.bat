37b6f0009afb6f695d0c451902da22c064dfe407
gm4 <yigitturkogluyum@gmail.com>
2025-05-27 21:30:44 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,1 +1,12 @@
-:: For these you need Python.
+@echo off
+python --version >nul 2>&1
+if errorlevel 1 (
+    echo Python is not installed. Please install Python and try again.
+    pause
+    exit /b
+)
+
+pip install os
+pip install sys
+pip install ctypes
+pip install shutil
